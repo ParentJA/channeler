4468a5536069fd3aff16b774269c0941bbad6d77
Jason Parent <jparent@eab.com>
2020-03-05 23:22:01 -0500

    Updates code

---
+++ client-vue/src/components/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+<fragment>
+  <h2>Tasks</h2>
+  <b-table-simple>
+    <b-thead>
+      <b-tr>
+        <b-th scope='col'>ID</b-th>
+        <b-th scope='col'>Status</b-th>
+        <b-th scope='col'>Created</b-th>
+        <b-th scope='col'>Updated</b-th>
+      </b-tr>
+    </b-thead>
+    <b-tbody v-if='tasks.length > 0'>
+      <task-list-row v-for='task in tasks' :key='task.id' v-bind:task='task' />
+    </b-tbody>
+    <b-tbody v-if='tasks.length === 0'>
+      <b-tr>
+        <b-td class='text-center' colspan='4'>No tasks.</b-td>
+      </b-tr>
+    </b-tbody>
+  </b-table-simple>
+</fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment'
+import TaskListRow from './TaskListRow'
+export default {
+  name: 'TaskList',
+  components: { Fragment, TaskListRow },
+  props: {
+    tasks: Array
+  }
+}
+</script>
--- client-vue/src/main.js
+++ client-vue/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import { BootstrapVue } from 'bootstrap-vue'
+import VueMoment from 'vue-moment'
+import Toasted from 'vue-toasted'
 import App from './App.vue'
 
 import 'bootswatch/dist/lumen/bootstrap.css'
@@ -7,6 +9,8 @@ import 'bootswatch/dist/lumen/bootstrap.css'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(VueMoment)
+Vue.use(Toasted, { duration: 1500 })
 
 new Vue({
   render: h => h(App),
